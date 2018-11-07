e74dceb115e97196e83a97036548fb7097d85dc1
shota.watanabe <t8mmmxc10y@yahoo.co.jp>
2018-11-07 11:14:06 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,14 +1,18 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading text-center">
-                            <img src="{{ $item->image_url }}" alt="">
+                            <img src="{{ $item->image_url }}" alt="" class="">
                         </div>
                         <div class="panel-body">
-                            <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @if ($item->id)
+                                <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                            @else
+                                <p class="item-title">{{ $item->name }}</p>
+                            @endif
                             <div class="buttons text-center">
                                 @if (Auth::check())
                                     @include('items.want_button', ['item' => $item])
@@ -16,6 +20,11 @@
                                 @endif
                             </div>
                         </div>
+                        @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
